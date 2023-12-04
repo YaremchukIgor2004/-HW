fec033a22fec094d133569aee62c70104774ae44
YaremchukIgor2004 <145205562+YaremchukIgor2004@users.noreply.github.com>
2023-12-04 20:13:36 +0200

    Add files via upload

---
+++ 4/hw4_1/hw4_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <math.h>
+//4.27.ж
+
+
+double calculatePi(double epsilon) {
+    double pi = 0.0;
+    double term = 1.0;
+    int k = 1;
+
+    while (term >= epsilon) {
+        term = 0.0;
+        for (int m = 1; m <= k; ++m) {
+            term += 1.0 / pow((4 * m - 2), k);
+        }
+
+        pi += term;
+        ++k;
+    }
+
+    return pi * 8;
+}
+
+int main() {
+    double epsilon;
+    printf("Введіть точність (ε): ");
+    scanf("%lf", &epsilon);
+
+    double result = calculatePi(epsilon);
+
+    printf("Pi з точністю %.15lf: %.15lf\n", epsilon, result);
+
+    return 0;
+}
