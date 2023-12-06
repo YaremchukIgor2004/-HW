e949c43215193d3fc605c16d21c15530f0e6dd0d
YaremchukIgor2004 <145205562+YaremchukIgor2004@users.noreply.github.com>
2023-12-06 14:49:43 +0200

    Changed lab4 CW and HW

---
+++ 4/CW4/8.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+//Task 8
+
+int main(void) {
+    int k = 0, m;
+    scanf("%d", &m);
+
+    int i = 1;
+    while (i * 4 <= m) {
+        i *= 4;
+        k++;
+    }
+
+    printf("%d\n", k);
+
+    i = 1;
+    k = 0;
+    do {
+        k++;
+        i *= 4;
+    } while (i <= m);
+
+    printf("%d\n", k - 1);
+}
