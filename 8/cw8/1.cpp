581a95cf7a4e4f7d2d759599aaad08fb8bba13eb
YaremchukIgor2004 <145205562+YaremchukIgor2004@users.noreply.github.com>
2023-12-06 14:54:32 +0200

    Changed lab8 CW and HW

---
+++ 8/cw8/1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Lab 9 Task 1
+
+int main(void) {
+    unsigned n;
+    scanf("%u", &n);
+    double* arr = (double*)malloc(n * sizeof(double));
+
+    if (!arr)
+        return EXIT_FAILURE;
+
+    for (unsigned i = 0; i < n; i++) {
+        scanf("%lf", arr + i);
+    }
+
+    double sqr_sum = 0.;
+    for (unsigned i = 0; i < n; i++)
+        sqr_sum += arr[i] * arr[i];
+
+    printf("%lf\n", sqr_sum);
+
+    free(arr);
+}
